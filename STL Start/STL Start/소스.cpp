7ac3db8cb6eee48f09bb348a81b427754dd87277
Choi_hae_seong <chs000218@naver.com>
2022-05-12 12:19:37 +0900

    2022-05-12

--- STL Start/STL Start/소스.cpp
+++ STL Start/STL Start/소스.cpp
@@ -1,18 +1,13 @@
 //-----------------------------------------------------------------------------
-// 2022. 1학기 STL 5월 11일 목요일(10주 2일)
+// 2022. 1학기 STL 5월 12일 목요일(11주 1일)
 // 
-// Associative Container - set, multi_set, map, multi_map
-// 
-// map<key, value, key_compare>
-//		- 연관배열(associative array)로 사용할 수 있다.
-// 
-// => 코딩하면서 에러를 더 줄이기 위해
-// 
+// Unoredered Associative Container - set, multi_set, map, multi_map
+//	- 이것보다 빨리 찾을 수 없다.
 // 5/25(수) - 졸업연구설명회(6교시 2:30 ~ 3:20)
 //-----------------------------------------------------------------------------
 
 #include <iostream>
-#include <map>
+#include <unordered_set>
 #include "save.h"
 #include "STRING.h"
 
@@ -21,6 +16,6 @@ int main()
 //----
 {
 	// save("소스.cpp");
-	// [문제] 소설에 사용된 알파벳의 빈도수를 출력하라.
+	
 	
 }
