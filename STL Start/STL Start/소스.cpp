2968c20698ae11049fd87174e377b3a84f051d77
Choi_hae_seong <chs000218@naver.com>
2022-06-09 12:17:49 +0900

    2022-06-09

--- STL Start/STL Start/소스.cpp
+++ STL Start/STL Start/소스.cpp
@@ -10,57 +10,94 @@
 #include <string>
 #include <fstream>
 #include <algorithm>
+#include <list>
 #include <vector>
+#include <ranges>
 #include "save.h"
 #include "STRING.h"
+
+
+// 벡터가 단어 쌍을 저장할 수 있도록 한다.
+// first는 정렬하고 second는 원래 단어를 저장
+
+struct WP : public std::pair<std::string, std::string> {
+	WP(const std::string& s) : std::pair<std::string, std::string>{ s,s } {
+		std::sort(first.begin(), first.end());
+	}
+};
 
 //---------
 int main()
 //---------
 {
-	//save("소스.cpp");
+	// save("소스.cpp");
 	// [문제] "단어들.txt"에 있는 단어를 읽고 갯수를 출력하라.
 		
 	std::ifstream in{ "단어들.txt" };
 
 	// std::string의 operator <<가 오버로딩 되어있기 때문에 사용가능
-	std::vector<std::string> word{ std::istream_iterator<std::string> {in}, {} };
-	std::cout << word.size() << std::endl;
+	std::vector<WP> v{ std::istream_iterator<std::string> {in}, {} };
+	std::cout << "단어의 개수 - " << v.size() << std::endl;
+	
+	// v에 저장된 WP를 출력해본다.
+	// 30000번째부터 100개를 출력하라
+	/*for (auto [정렬, 원본] : v | std::views::drop(30000)
+								  | std::views::take(100)) {
+		std::cout << 정렬 << " ---- " << 원본 << std::endl;
+	}*/
 
-	// 정렬되어 있나 확인해보자
+	/*std::sort(v.begin(), v.end(), [](const WP& a, const WP& b) {
+		return a.first < b.first;
+	});*/
 
-	// 세번째 인자로 ? 기준으로 정렬되어있나 확인해보자!
-	std::cout << "정렬? "<< std::boolalpha << std::is_sorted(word.begin(), word.end(), std::less<std::string>()) << std::endl;
+	std::ranges::sort(v, {}, &WP::first);
 
-	// 중복이 있나 체크해보자!
-	auto p = std::unique(word.begin(), word.end());
-	if (p == word.end())
-		std::cout << "중복X" << std::endl;
-	else
-		std::cout << "중복O" << std::endl;
+	/*for (auto [정렬, 원본] : v | std::views::drop(30000)
+		| std::views::take(100)) {
+		std::cout << 정렬 << " ---- " << 원본 << std::endl;
+	}*/
 
-	// [문제] 가장 긴 단어를 출력하라.
-	p = std::max_element(word.begin(), word.end(), [](const std::string& a, const std::string& b) {
-		return a.size() < b.size();  
-	});
-	std::cout << "길이 " <<  p->size() << ", " << *p << std::endl;
+	// [문제] 모든 anagram을 출력하라.
+	auto b = v.begin();
+	int cnt{};
+	//std::ofstream out{"애너그램들.txt"};
+	
+	std::vector<std::list<std::string>> vv;
+	vv.reserve(31000);
 
+	// [문제] 애너그램 쌍이 가장 많은 것을 찾아라
+	while (true) {
+		auto i = std::adjacent_find(b, v.end(), [](const WP& a, const WP& b) {
+			return a.first == b.first;
+		});
 
-	// [과제] 사용자 입력한 단어가 단어들에 있는지 확인한다.
-	// 있다면 anagram을 찾아 화면에 출력하라.
-	while (true){
-		std::string s;
-		std::cout << "찾을 단어는? ";
-		std::cin >> s;
-		
-		if (!std::binary_search(word.begin(), word.end(), s)) {
-			std::cout << "단어가 없습니다." << std::endl;
-			continue;
-		}
+		if (i == v.end())
+			break;
 
-		// 애너그램을 찾아 출력한다.
-		// word의 각 순열이 단어들에 있는지 확인한다.
-		
-		
+		auto j = std::find_if_not(i + 1, v.end(), [i](const WP& a) {
+			return a.first == i->first;
+		});
+
+		// [i, j]는 애너그램 구간이다.
+		std::list<std::string> cont;
+		for (auto p = i; p < j; ++p)
+			cont.push_back(p->second);
+		vv.push_back(cont);
+		b = j;
 	}
+
+	// vv를 list의 갯수 순으로 내림차순 정렬
+	std::sort(vv.begin(), vv.end(), [](const std::list<std::string>& a, const std::list<std::string>& b) {
+		return a.size() > b.size();
+	});
+
+	std::cout << "애너그램 많은 수 - " << vv.begin()->size() << std::endl;
+
+	for (const std::list<std::string>& cont : vv | std::views::take(10))
+	{
+		for (const std::string s : cont)
+			std::cout << s << " ";
+		std::cout << std::endl;
+	}
+
 }
